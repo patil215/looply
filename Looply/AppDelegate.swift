46e5756306b55c69514d8c78bd917b3458efc936
Ehsan Asdar <ehsanmasdar@gmail.com>
2016-10-08 17:03:49 -0400

    Initial Commit

---
+++ Looply/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Looply
+//
+//  Created by Ehsan Asdar on 10/8/16.
+//  Copyright © 2016 Ehsan and Neil. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Looply/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Looply
+//
+//  Created by Ehsan Asdar on 10/8/16.
+//  Copyright © 2016 Ehsan and Neil. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
