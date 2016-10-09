fdf1aa13085a5eac7fc90dbf74e6cb78ccd715b1
Ehsan Asdar <ehsanmasdar@gmail.com>
2016-10-08 20:41:23 -0400

    Implement linear volume scaling

--- Looply/ViewController.swift
+++ Looply/ViewController.swift
@@ -12,9 +12,36 @@ import AudioToolbox
 
 class ViewController: NSViewController {
     
+    // TODO must prompt user to remove ambient noise detection? (or turn this flag off)
+    
     var mic : AKMicrophone? = nil
     var tracker : AKFrequencyTracker? = nil
     var silence : AKBooster? = nil
+    
+    var lowerMicBound: Double = 0.0
+    var upperMicBound: Double = 0.75
+    var lowerOutputBound: Double = 1/24
+    var upperOutputBound: Double = /*0.5*/ 1
+    var scaleFactor: Double = 1
+    
+    func getOutputVolume (micLevel : Double) -> Double {
+        if micLevel > upperMicBound {
+            return upperOutputBound
+        }
+        if micLevel < lowerMicBound {
+            return lowerOutputBound
+        }
+        
+        let slope = ((upperOutputBound - lowerOutputBound) / (upperMicBound - lowerMicBound)) * scaleFactor
+        let resultingVolume = (slope * (micLevel - lowerMicBound)) + lowerOutputBound
+        if resultingVolume > upperMicBound {
+            return upperOutputBound
+        }
+        if(resultingVolume < lowerMicBound) {
+            return lowerOutputBound
+        }
+        return resultingVolume
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,25 +56,55 @@ class ViewController: NSViewController {
     override func viewDidAppear() {
         AudioKit.output = silence;
         AudioKit.start()
-        
         while(true) {
-            updateUI()
-            usleep(200)
+            var sum = Double(0)
+            for i in 0...1000 {
+                sum = sum + tracker!.amplitude
+                usleep(2000)
+            }
+            updateVolume(amplitude: getOutputVolume(micLevel: (sum / 1000)))
+            print(getOutputVolume(micLevel: (sum / 1000)))
+            print(sum / 1000)
         }
     }
+    
     func updateUI(){
         print("hi")
-        print(tracker?.amplitude)
+        updateVolume(amplitude: tracker!.amplitude)
     }
     
-    func getDeviceVolume() {
-        var deviceID = AudioDeviceID(0)
-        var deviceIDSize = UInt32(sizeofValue(deviceID))
-        var devicePropertyAddress = AudioObjectPropertyAddress(mSelectof)
-    }
-    
-    func getVolume() {
+    func updateVolume(amplitude : Double) {
+        var defaultOutputDeviceID = AudioDeviceID(0)
+        var defaultOutputDeviceIDSize = UInt32(MemoryLayout.size(ofValue: defaultOutputDeviceID))
         
+        var getDefaultOutputDevicePropertyAddress = AudioObjectPropertyAddress(
+            mSelector: AudioObjectPropertySelector(kAudioHardwarePropertyDefaultOutputDevice),
+            mScope: AudioObjectPropertyScope(kAudioObjectPropertyScopeGlobal),
+            mElement: AudioObjectPropertyElement(kAudioObjectPropertyElementMaster))
+        
+        let status1 = AudioObjectGetPropertyData(
+            AudioObjectID(kAudioObjectSystemObject),
+            &getDefaultOutputDevicePropertyAddress,
+            0,
+            nil,
+            &defaultOutputDeviceIDSize,
+            &defaultOutputDeviceID)
+        
+        var volume = Float32(amplitude) // 0.0 ... 1.0
+        let volumeSize = UInt32(MemoryLayout.size(ofValue: volume))
+        
+        var volumePropertyAddress = AudioObjectPropertyAddress(
+            mSelector: AudioObjectPropertySelector(kAudioHardwareServiceDeviceProperty_VirtualMasterVolume),
+            mScope: AudioObjectPropertyScope(kAudioDevicePropertyScopeOutput),
+            mElement: AudioObjectPropertyElement(kAudioObjectPropertyElementMaster))
+        
+        let status2 = AudioHardwareServiceSetPropertyData(
+            defaultOutputDeviceID,
+            &volumePropertyAddress,
+            0,
+            nil,
+            volumeSize,
+            &volume)
     }
     
 
