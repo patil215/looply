668d4650cdf64b1b6ae64623660f269c7b391250
patil215 <neilpatil215@gmail.com>
2016-10-09 05:01:34 -0400

    Use sliders for adjustment and add test button

--- Looply/PreferencesWindow.swift
+++ Looply/PreferencesWindow.swift
@@ -7,14 +7,19 @@
 //
 
 import Cocoa
-
+import AudioToolbox
+import AVFoundation
 
 class PreferencesWindow: NSWindowController {
 
-    @IBOutlet weak var lowerOutputBound: NSTextField!
-    @IBOutlet weak var upperOutputBound: NSTextField!
-    @IBOutlet weak var scaleFactor: NSTextField!
+    
+    @IBOutlet weak var upperOutputBoundButton: NSButton!
+    @IBOutlet weak var lowerOutputBoundButton: NSButton!
+    @IBOutlet weak var scaleFactor: NSSlider!
+    @IBOutlet weak var upperOutputBound: NSSlider!
+    @IBOutlet weak var lowerOutputBound: NSSlider!
     var storage : Storage = Storage()
+    var backgroundMusicPlayer = AVAudioPlayer()
 
     override var windowNibName : String! {
         return "PreferencesWindow"
@@ -22,21 +27,85 @@ class PreferencesWindow: NSWindowController {
     
     override func windowDidLoad() {
         super.windowDidLoad()
-        lowerOutputBound.doubleValue = storage.getMinPoint()[1]
-        upperOutputBound.doubleValue = storage.getMaxPoint()[1]
-        scaleFactor.doubleValue = storage.getScaleFactor()
+        lowerOutputBound.doubleValue = storage.getMinPoint()[1] * 100
+        upperOutputBound.doubleValue = storage.getMaxPoint()[1] * 100
+        scaleFactor.doubleValue = storage.getScaleFactor() * 100
         self.window?.center()
         self.window?.makeKeyAndOrderFront(nil)
         NSApp.activate(ignoringOtherApps: true)
     }
+    
+    func updateVolume(amplitude : Double) {
+        var defaultOutputDeviceID = AudioDeviceID(0)
+        var defaultOutputDeviceIDSize = UInt32(MemoryLayout.size(ofValue: defaultOutputDeviceID))
+        
+        var getDefaultOutputDevicePropertyAddress = AudioObjectPropertyAddress(
+            mSelector: AudioObjectPropertySelector(kAudioHardwarePropertyDefaultOutputDevice),
+            mScope: AudioObjectPropertyScope(kAudioObjectPropertyScopeGlobal),
+            mElement: AudioObjectPropertyElement(kAudioObjectPropertyElementMaster))
+        
+        let status1 = AudioObjectGetPropertyData(
+            AudioObjectID(kAudioObjectSystemObject),
+            &getDefaultOutputDevicePropertyAddress,
+            0,
+            nil,
+            &defaultOutputDeviceIDSize,
+            &defaultOutputDeviceID)
+        
+        var volume = Float32(amplitude) // 0.0 ... 1.0
+        let volumeSize = UInt32(MemoryLayout.size(ofValue: volume))
+        
+        var volumePropertyAddress = AudioObjectPropertyAddress(
+            mSelector: AudioObjectPropertySelector(kAudioHardwareServiceDeviceProperty_VirtualMasterVolume),
+            mScope: AudioObjectPropertyScope(kAudioDevicePropertyScopeOutput),
+            mElement: AudioObjectPropertyElement(kAudioObjectPropertyElementMaster))
+        
+        let status2 = AudioHardwareServiceSetPropertyData(
+            defaultOutputDeviceID,
+            &volumePropertyAddress,
+            0,
+            nil,
+            volumeSize,
+            &volume)
+    }
+    
+    
+    func playBackgroundMusic(filename: String) {
+        let url = Bundle.main.url(forResource: filename, withExtension: nil)
+        guard let newURL = url else {
+            print("Could not find file: \(filename)")
+            return
+        }
+        do {
+            backgroundMusicPlayer = try AVAudioPlayer(contentsOf: newURL)
+            backgroundMusicPlayer.numberOfLoops = 1
+            backgroundMusicPlayer.prepareToPlay()
+            backgroundMusicPlayer.play()
+        } catch let error as NSError {
+            print(error.description)
+        }
+    }
+    
+    @IBAction func onTestUpperOutputBoundChange(_ sender: AnyObject) {
+        updateVolume(amplitude: upperOutputBound.doubleValue / 100)
+        playBackgroundMusic(filename: "Stirling.wav")
+    }
+    
+    @IBAction func onTestLowerOutputBoundChange(_ sender: AnyObject) {
+        updateVolume(amplitude: lowerOutputBound.doubleValue / 100)
+        playBackgroundMusic(filename: "Stirling.wav")
+    }
+    
     @IBAction func onLowerOutputBoundChange(_ sender: AnyObject) {
-        storage.setMinPoint(minPoint: [0, lowerOutputBound.doubleValue])
+        storage.setMinPoint(minPoint: [0, lowerOutputBound.doubleValue / 100])
+        print(lowerOutputBound.doubleValue / 100)
     }
+    
     @IBAction func onUpperOutputBoundChange(_ sender: AnyObject) {
-        storage.setMaxPoint(maxPoint: [0.75, upperOutputBound.doubleValue])
-
+        storage.setMaxPoint(maxPoint: [0.75, upperOutputBound.doubleValue / 100])
     }
+    
     @IBAction func onScaleFactorChange(_ sender: AnyObject) {
-        storage.setScaleFactor(scaleFactor: scaleFactor.doubleValue)
+        storage.setScaleFactor(scaleFactor: scaleFactor.doubleValue / 100)
     }
 }
